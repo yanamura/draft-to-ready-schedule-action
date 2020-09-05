7a48b71762268ed5df517c95a073a13adfaa144c
Yasuharu Yanamura <yanamura3@gmail.com>
2020-09-05 17:40:25 +0900

    get pull requests

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,24 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const octokit = github.getOctokit(core.getInput('github_token'))
+
+    const {data: pullRequestList} = await octokit.pulls.list({
+      ...github.context.repo,
+      state: 'open'
+    })
+
+    core.debug(pullRequestList.toString())
+    /*const ms: string = core.getInput(''')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', new Date().toTimeString())*/
   } catch (error) {
     core.setFailed(error.message)
   }
